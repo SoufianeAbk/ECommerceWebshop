1b4f10eed0b43fee05f2ee8623c90a948d9ff75f
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-10-10 18:28:22 +0200

    Updates HomeController.

--- ECommerceWebshop/Controllers/HomeController.cs
+++ ECommerceWebshop/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
 using System.Diagnostics;
 using ECommerceWebshop.Models;
+using ECommerceWebshop.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceWebshop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewData["FeaturedProducts"] = await _context.Products
+                .Include(p => p.Category)
+                .Take(8)
+                .ToListAsync();
+
+            ViewData["Categories"] = await _context.Categories.ToListAsync();
+
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
