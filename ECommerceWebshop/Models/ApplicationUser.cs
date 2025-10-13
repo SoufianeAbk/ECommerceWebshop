1e6cfb7e01a0bdc5b1a69a7f58153f97418e0252
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-10-13 16:21:50 +0200

    Nieuw Model Aangemaakt.

---
+++ ECommerceWebshop/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceWebshop.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [StringLength(200)]
+        public string? Address { get; set; }
+
+        [StringLength(100)]
+        public string? City { get; set; }
+
+        [StringLength(10)]
+        public string? PostalCode { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
+        // Navigation property voor orders
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
---
+++ ECommerceWebshop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceWebshop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-mail is verplicht")]
+        [EmailAddress(ErrorMessage = "Ongeldig e-mailadres")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Wachtwoord is verplicht")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Onthoud mij")]
+        public bool RememberMe { get; set; }
+    }
+}
